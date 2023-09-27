9c55b8d39b1492b72bd408d473db11440b18a791
Key <213482@ids.upchiapas,edu.mx>
2023-09-27 12:59:53 -0600

    main

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 // main.go
 package main
 
+import (
+	"carreras/game"
+	"log"
+	"math/rand"
+	"time"
 
+	"github.com/hajimehoshi/ebiten/v2"
+)
 
 func main() {
-	
+	rand.Seed(time.Now().UnixNano())
+
+	// Crear la ventana del juego
+	ebiten.SetWindowSize(game.ScreenWidth, game.ScreenHeight)
+	ebiten.SetWindowTitle("MARIO")
+
+	g := game.NewGame()
+	if err := ebiten.RunGame(g); err != nil {
+		log.Fatal(err)
+	}
 }
